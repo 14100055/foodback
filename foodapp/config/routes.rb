f5958e9bd10c5b384790b3509e260a2e68785997
14100055 <saad.nismail@gmail.com>
2016-03-02 10:26:26 +0000

    Implemented signup/login

--- foodapp/config/routes.rb
+++ foodapp/config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+
+  get 'signup' => 'users#new'
+  resources :users
+  
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  resources :sessions
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

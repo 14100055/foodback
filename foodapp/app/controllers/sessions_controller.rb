1006c090a8391361c67614930a180e42a1abef96
14100055 <saad.nismail@gmail.com>
2016-03-13 10:25:36 +0000

    Basic meal plan (breakfast)

--- foodapp/app/controllers/sessions_controller.rb
+++ foodapp/app/controllers/sessions_controller.rb
@@ -24,13 +24,39 @@ class SessionsController < ApplicationController
     
     def compute
         @id = session[:user_id]
-        User.update(@id, :budget => params[:session][:budget])
+        @user = User.find(@id)
+
+        if @user.update(:budget => params[:session][:budget])
+            budget = params[:session][:budget].to_i
+            plan = get_plan(budget)
+            @user.update(:plan => plan)
+        end
         redirect_to '/mainpage'
     end
     
     def destroy
         session[:user_id] = nil
         redirect_to '/login'
+    end
+
+    def get_plan(budget)
+        @morning = Food.where(:start => Time.utc(2000,01,01, 7,00,00)...Time.utc(2000,01,01, 11,00,00))
+        morning_money = budget/3
+        m_place = Array.new
+        m_foods = Array.new
+        @morning.each do |food|
+            if food.price < morning_money
+                m_place.append(food.restaurant)
+                m_foods.append(food.name)
+            end
+        end
+        len = m_foods.size
+        index = rand(len)
+        place = m_place.at(index)
+        foods = m_foods.at(index)
+
+        plan = "BREAKFAST: #{place} - #{foods}"
+        return plan
     end
 
 end

d1c9095d937bb1ce38aaa84a7a1342423416988d
14100055 <saad.nismail@gmail.com>
2016-03-26 03:45:31 +0000

    Debugging 3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -36,20 +36,20 @@ class SessionsController < ApplicationController
 
             b_start = Time.utc(2000,01,01, 7,00,00)
             b_finish = Time.utc(2000,01,01, 11,00,00)
-            @breakfast = "PDC,Omelette,30\nPDC,Bread,10"
-            # @breakfast = get_plan(budget, b_start, b_finish)
+            # @breakfast = "PDC,Omelette,30\nPDC,Bread,10"
+            @breakfast = get_plan(budget, b_start, b_finish)
             @user.update(:breakfast => @breakfast)
 
             l_start = Time.utc(2000,01,01, 12,30,00)
             l_finish = Time.utc(2000,01,01, 16,00,00)
-            @lunch = "PDC,Omelette,30\nPDC,Bread,10"
-            # @lunch = get_plan(budget, l_start, l_finish)
+            # @lunch = "PDC,Omelette,30\nPDC,Bread,10"
+            @lunch = get_plan(budget, l_start, l_finish)
             @user.update(:lunch => @lunch)
 
             d_start = Time.utc(2000,01,01, 19,00,00)
             d_finish = Time.utc(2000,01,01, 23,00,00)
-            @dinner = "PDC,Omelette,30\nPDC,Bread,10"
-            # @dinner = get_plan(budget, d_start, d_finish)
+            # @dinner = "PDC,Omelette,30\nPDC,Bread,10"
+            @dinner = get_plan(budget, d_start, d_finish)
             @user.update(:dinner => @dinner)
 
         end

2df8631b914ddd08acc4a3da3f986452165face2
14100055 <saad.nismail@gmail.com>
2016-05-05 03:26:42 +0000

    Small additions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
     validates :first_name, presence: true, length: {minimum: 1}
     validates :last_name, presence: true, length: {minimum: 1}
     validates :email, presence: true, length: {minimum: 1}, uniqueness: true
-    validates :budget, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0}
+    validates :month_budget, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0}
+    validates :budget, :numericality => {:only_integer => true}
     has_secure_password
     
     def self.all_attributes
